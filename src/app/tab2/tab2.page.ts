b41a09a6d5e551543988116f7ea6248d1846cc72
Thomas Degroot <thomas.degroot@veriforce.com>
2025-09-24 14:05:22 -0600

    add places and search radius

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,4 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { GeolocationService } from '../services/geolocation.service';
+import { JsonPipe } from '@angular/common';
+import { PlacesService } from '../services/places.service';
+import { FormsModule } from '@angular/forms';
+
 
 @Component({
   selector: 'app-tab2',
@@ -6,8 +11,67 @@ import { Component } from '@angular/core';
   styleUrls: ['tab2.page.scss'],
   standalone: false,
 })
-export class Tab2Page {
+export class Tab2Page implements OnInit, OnDestroy{
+  currentLocation: any = null;
+  geofenceEvents: any[] = [];
+  searchQuery: string = '';
+  searchResults: any[] = [];
 
-  constructor() {}
+  constructor(
+    private geoService: GeolocationService,
+    private placesService: PlacesService
+  ) { }
+
+    async ngOnInit() {
+    // Get current location
+    this.currentLocation = await this.geoService.getCurrentPosition();
+
+    // Start watching location
+    await this.geoService.startWatchingLocation((loc) => {
+      this.currentLocation = loc;
+    });
+
+    // Listen for geofence triggers
+    this.geoService.onGeofenceEvent((event) => {
+      this.geofenceEvents.push(event);
+    });
+
+    // Add a sample geofence (Google HQ in Mountain View)
+    this.geoService.addGeofence('GoogleHQ', 37.422, -122.084, 200);
+  }
+
+   async ngOnDestroy() {
+    await this.geoService.stopWatchingLocation();
+  }
+
+    // Search places
+  search() {
+    if (this.searchQuery.trim().length === 0) {
+      this.searchResults = [];
+      return;
+    }
+    this.placesService.searchPlaces(this.searchQuery).subscribe((res: any) => {
+      this.searchResults = res.predictions;
+    });
+  }
+
+    // Select a place and add geofence
+  selectPlace(place: any) {
+    this.placesService.getPlaceDetails(place.place_id).subscribe((res: any) => {
+      const location = res.result.geometry.location;
+      this.geoService.addGeofence(
+        place.description,
+        location.lat,
+        location.lng,
+        200
+      );
+      alert(`Geofence added for ${place.description}`);
+      this.searchResults = [];
+      this.searchQuery = '';
+    });
+  }
+
+
+
 
 }
